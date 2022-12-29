23132c4ed3bb6cde22a9f03c051da07dcb19ac5c
ValentasJ <vjankauskas92@gmail.com>
2022-12-29 15:48:00 +0200

    DataBase implementation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contacts;
 
 
 class IndexController extends AbstractController
@@ -11,7 +14,7 @@ class IndexController extends AbstractController
     #[Route('/', name: 'base')]
     public function homePage()
     {
-        return $this->render('bodyeyelashes.html.twig');
+        return $this->render('bodymyservices.html.twig');
     }
 
     #[Route('/eyelashes', name: 'eyelashes')]
@@ -26,16 +29,79 @@ class IndexController extends AbstractController
         return $this->render('bodyaboutme.html.twig');
     }
 
-    #[Route('/contacts', name: 'contacts')]
-    public function contactsPage()
-    {
-        return $this->render('bodycontacts.html.twig');
-    }
-
     #[Route('/my-services', name: 'myservices')]
     public function myServicesPage()
     {
         return $this->render('bodymyservices.html.twig');
     }
 
+    #[Route('/contacts', name: 'contacts')]
+    function contactPage(ManagerRegistry $doctrine)
+    {
+        $request = Request::createFromGlobals();
+        $clientName = $request->request->get('client-name');
+        $clientEmail = $request->request->get('client-email');
+        $message = $request->request->get('client-message');
+
+
+
+        if ($request->isMethod('POST')) {
+            $error = null;
+            if (!$clientName || !$clientEmail || !$message) {
+                $error = 'Client name, email or message was not set';
+            }
+
+            if (strlen($clientEmail) < 3) {
+                $error = 'Email is less than 3 symbols';
+            }
+
+            if ($error) {
+                return $this->render('bodycontacts.html.twig', [
+                    'error' => $error
+                ]);
+            }
+
+            $contact = new Contacts();
+
+            $contact->setEmail($clientEmail);
+            $contact->setName($clientName);
+            $contact->setMessage($message);
+
+            $manager = $doctrine->getManager();
+
+            $manager->persist($contact);
+
+            $manager->flush();
+
+            $this->addFlash('success', true);
+
+            return $this->redirectToRoute('contacts');
+        }
+
+        return $this->render('bodycontacts.html.twig');
+    }
+
+    #[Route('/admin', name: 'admin')]
+    function admin(ManagerRegistry $doctrine)
+    {
+        $contactRepository = $doctrine->getManager()->getRepository(Contacts::class);
+
+        $contacts = $contactRepository->findAll();
+
+        return $this->render('admin.html.twig', [
+            'contacts' => $contacts
+        ]);
+    }
+
+    #[Route('/admin/contact/{id}', name: 'view_contact')]
+    function viewContact(ManagerRegistry $doctrine, $id)
+    {
+        $contactRepository = $doctrine->getManager()->getRepository(Contacts::class);
+
+        $contact = $contactRepository->findOneBy(['id' => $id]);
+
+        return $this->render('single-contact.html.twig', [
+            'contact' => $contact
+        ]);
+    }
 }
---
+++ src/Entity/Contacts.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactsRepository::class)]
+class Contacts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->Message;
+    }
+
+    public function setMessage(string $Message): self
+    {
+        $this->Message = $Message;
+
+        return $this;
+    }
+}
